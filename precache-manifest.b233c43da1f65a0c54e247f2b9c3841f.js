a11729b061f6846ac0252c6623deb770f1bba3c3
Pablo Tapia <ptapia@proyecto404.com>
2019-09-10 21:17:40 -0300

    Updates

---
+++ precache-manifest.b233c43da1f65a0c54e247f2b9c3841f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b16b8f3e17ea4047319223584c3cecd3",
+    "url": "/zagora/index.html"
+  },
+  {
+    "revision": "b7be99624cd13a3e235c",
+    "url": "/zagora/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b48c38cc68a66823aff7",
+    "url": "/zagora/static/js/2.a8f967ce.chunk.js"
+  },
+  {
+    "revision": "b7be99624cd13a3e235c",
+    "url": "/zagora/static/js/main.da590aaf.chunk.js"
+  },
+  {
+    "revision": "b79aeb216528a7e07ce2",
+    "url": "/zagora/static/js/runtime~main.971f514e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/zagora/static/media/logo.5d5d9eef.svg"
+  }
+]);
