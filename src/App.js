5aef57d9c02d9388b3dc22d833a6ee4912dc0384
Pablo Tapia <ptapia@proyecto404.com>
2019-09-10 22:12:47 -0300

    cumples

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class='container'>
+
+        <FullCalendar defaultView="dayGridMonth" plugins={[dayGridPlugin]} locale="es" events={[
+          { title: 'Cumple Santi', date: '2019-01-24' },
+          { title: 'Cumple Blue', date: '2019-03-30' },
+          { title: 'Cumple Ed', date: '2019-08-11' },
+          { title: 'Cumple Andy', date: '2019-08-19' },
+          { title: 'Cumple Seba', date: '2019-09-28' },
+          { title: 'Cumple Trish', date: '2019-10-17' },
+          { title: 'Cumple Peibl', date: '2019-10-24' },
+          { title: 'Cumple Ari', date: '2019-12-04' },
+          { title: 'Cumple Kristell', date: '2019-12-09' },
+          { title: 'Cumple Santi', date: '2020-01-24' },
+          { title: 'Cumple Blue', date: '2020-03-30' },
+          { title: 'Cumple Ed', date: '2020-08-11' },
+          { title: 'Cumple Andy', date: '2020-08-19' },
+          { title: 'Cumple Seba', date: '2020-09-28' },
+          { title: 'Cumple Trish', date: '2020-10-17' },
+          { title: 'Cumple Peibl', date: '2020-10-24' },
+          { title: 'Cumple Ari', date: '2020-12-04' },
+          { title: 'Cumple Kristell', date: '2020-12-09' }
+        ]} />
+      </div>
     </div>
   );
 }
